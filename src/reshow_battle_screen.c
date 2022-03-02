9ef17be723f01a3c1791cbe6aaae2757107f3e62
Lawrence Li <16lawrencel@gmail.com>
2022-03-01 23:41:37 -0500

    Display battle order icons after coming back from party menu

--- src/reshow_battle_screen.c
+++ src/reshow_battle_screen.c
@@ -15,6 +15,7 @@
 #include "battle_interface.h"
 #include "battle_anim.h"
 #include "data.h"
+#include "battle_order.h"
 
 // this file's functions
 static void CB2_ReshowBattleScreenAfterMenu(void);
@@ -182,6 +183,9 @@ static void CB2_ReshowBattleScreenAfterMenu(void)
                 CreateWirelessStatusIndicatorSprite(0, 0);
             }
         }
+        break;
+    case 26:
+        CreateAllBattleOrderMonIconSprites();
         break;
     default:
         SetVBlankCallback(VBlankCB_Battle);
