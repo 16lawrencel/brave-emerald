pret/pokeemerald

Fix battle send-out messages for triple battle so that it says all three pokemon are sent out

include/battle_message.h
@@ -13,50 +13,52 @@
 #define B_TXT_OPPONENT_MON1_NAME 0x6
 #define B_TXT_PLAYER_MON2_NAME 0x7
 #define B_TXT_OPPONENT_MON2_NAME 0x8
-#define B_TXT_LINK_PLAYER_MON1_NAME 0x9
-#define B_TXT_LINK_OPPONENT_MON1_NAME 0xA
-#define B_TXT_LINK_PLAYER_MON2_NAME 0xB
-#define B_TXT_LINK_OPPONENT_MON2_NAME 0xC
-#define B_TXT_ATK_NAME_WITH_PREFIX_MON1 0xD
-#define B_TXT_ATK_PARTNER_NAME 0xE
-#define B_TXT_ATK_NAME_WITH_PREFIX 0xF
-#define B_TXT_DEF_NAME_WITH_PREFIX 0x10
-#define B_TXT_EFF_NAME_WITH_PREFIX 0x11 // EFF = short for gEffectBank
-#define B_TXT_ACTIVE_NAME_WITH_PREFIX 0x12
-#define B_TXT_SCR_ACTIVE_NAME_WITH_PREFIX 0x13
-#define B_TXT_CURRENT_MOVE 0x14
-#define B_TXT_LAST_MOVE 0x15
-#define B_TXT_LAST_ITEM 0x16
-#define B_TXT_LAST_ABILITY 0x17
-#define B_TXT_ATK_ABILITY 0x18
-#define B_TXT_DEF_ABILITY 0x19
-#define B_TXT_SCR_ACTIVE_ABILITY 0x1A
-#define B_TXT_EFF_ABILITY 0x1B
-#define B_TXT_TRAINER1_CLASS 0x1C
-#define B_TXT_TRAINER1_NAME 0x1D
-#define B_TXT_LINK_PLAYER_NAME 0x1E
-#define B_TXT_LINK_PARTNER_NAME 0x1F
-#define B_TXT_LINK_OPPONENT1_NAME 0x20
-#define B_TXT_LINK_OPPONENT2_NAME 0x21
-#define B_TXT_LINK_SCR_TRAINER_NAME 0x22
-#define B_TXT_PLAYER_NAME 0x23
-#define B_TXT_TRAINER1_LOSE_TEXT 0x24
-#define B_TXT_TRAINER1_WIN_TEXT 0x25
-#define B_TXT_26 0x26
-#define B_TXT_PC_CREATOR_NAME 0x27
-#define B_TXT_ATK_PREFIX1 0x28
-#define B_TXT_DEF_PREFIX1 0x29
-#define B_TXT_ATK_PREFIX2 0x2A
-#define B_TXT_DEF_PREFIX2 0x2B
-#define B_TXT_ATK_PREFIX3 0x2C
-#define B_TXT_DEF_PREFIX3 0x2D
-#define B_TXT_TRAINER2_CLASS 0x2E
-#define B_TXT_TRAINER2_NAME 0x2F
-#define B_TXT_TRAINER2_LOSE_TEXT 0x30
-#define B_TXT_TRAINER2_WIN_TEXT 0x31
-#define B_TXT_PARTNER_CLASS 0x32
-#define B_TXT_PARTNER_NAME 0x33
-#define B_TXT_BUFF3 0x34
+#define B_TXT_PLAYER_MON3_NAME 0x9
+#define B_TXT_OPPONENT_MON3_NAME 0xA
+#define B_TXT_LINK_PLAYER_MON1_NAME 0xB
+#define B_TXT_LINK_OPPONENT_MON1_NAME 0xC
+#define B_TXT_LINK_PLAYER_MON2_NAME 0xD
+#define B_TXT_LINK_OPPONENT_MON2_NAME 0xE
+#define B_TXT_ATK_NAME_WITH_PREFIX_MON1 0xF
+#define B_TXT_ATK_PARTNER_NAME 0x10
+#define B_TXT_ATK_NAME_WITH_PREFIX 0x11
+#define B_TXT_DEF_NAME_WITH_PREFIX 0x12
+#define B_TXT_EFF_NAME_WITH_PREFIX 0x13 // EFF = short for gEffectBank
+#define B_TXT_ACTIVE_NAME_WITH_PREFIX 0x14
+#define B_TXT_SCR_ACTIVE_NAME_WITH_PREFIX 0x15
+#define B_TXT_CURRENT_MOVE 0x16
+#define B_TXT_LAST_MOVE 0x17
+#define B_TXT_LAST_ITEM 0x18
+#define B_TXT_LAST_ABILITY 0x19
+#define B_TXT_ATK_ABILITY 0x1A
+#define B_TXT_DEF_ABILITY 0x1B
+#define B_TXT_SCR_ACTIVE_ABILITY 0x1C
+#define B_TXT_EFF_ABILITY 0x1D
+#define B_TXT_TRAINER1_CLASS 0x1E
+#define B_TXT_TRAINER1_NAME 0x1F
+#define B_TXT_LINK_PLAYER_NAME 0x20
+#define B_TXT_LINK_PARTNER_NAME 0x21
+#define B_TXT_LINK_OPPONENT1_NAME 0x22
+#define B_TXT_LINK_OPPONENT2_NAME 0x23
+#define B_TXT_LINK_SCR_TRAINER_NAME 0x24
+#define B_TXT_PLAYER_NAME 0x25
+#define B_TXT_TRAINER1_LOSE_TEXT 0x26
+#define B_TXT_TRAINER1_WIN_TEXT 0x27
+#define B_TXT_26 0x28
+#define B_TXT_PC_CREATOR_NAME 0x29
+#define B_TXT_ATK_PREFIX1 0x2A
+#define B_TXT_DEF_PREFIX1 0x2B
+#define B_TXT_ATK_PREFIX2 0x2C
+#define B_TXT_DEF_PREFIX2 0x2D
+#define B_TXT_ATK_PREFIX3 0x2E
+#define B_TXT_DEF_PREFIX3 0x2F
+#define B_TXT_TRAINER2_CLASS 0x30
+#define B_TXT_TRAINER2_NAME 0x31
+#define B_TXT_TRAINER2_LOSE_TEXT 0x32
+#define B_TXT_TRAINER2_WIN_TEXT 0x33
+#define B_TXT_PARTNER_CLASS 0x34
+#define B_TXT_PARTNER_NAME 0x35
+#define B_TXT_BUFF3 0x36
 
 // for B_TXT_BUFF1, B_TXT_BUFF2 and B_TXT_BUFF3
 
@@ -206,7 +208,7 @@ struct BattleMsgData
     u8 hpScale;
     u8 itemEffectBattler;
     u8 moveType;
-    u8 abilities[MAX_BATTLERS_COUNT];
+    u8 abilities[MAX_BATTLERS_COUNT_2];
     u8 textBuffs[3][TEXT_BUFF_ARRAY_COUNT];
 };
 

16lawrencel/brave-emerald

include/battle_message.h
@@ -13,59 +13,61 @@
 #define B_TXT_OPPONENT_MON1_NAME 0x6
 #define B_TXT_PLAYER_MON2_NAME 0x7
 #define B_TXT_OPPONENT_MON2_NAME 0x8
-#define B_TXT_LINK_PLAYER_MON1_NAME 0x9
-#define B_TXT_LINK_OPPONENT_MON1_NAME 0xA
-#define B_TXT_LINK_PLAYER_MON2_NAME 0xB
-#define B_TXT_LINK_OPPONENT_MON2_NAME 0xC
-#define B_TXT_ATK_NAME_WITH_PREFIX_MON1 0xD
-#define B_TXT_ATK_PARTNER_NAME 0xE
-#define B_TXT_ATK_NAME_WITH_PREFIX 0xF
-#define B_TXT_DEF_NAME_WITH_PREFIX 0x10
-#define B_TXT_EFF_NAME_WITH_PREFIX 0x11 // EFF = short for gEffectBank
-#define B_TXT_ACTIVE_NAME_WITH_PREFIX 0x12
-#define B_TXT_SCR_ACTIVE_NAME_WITH_PREFIX 0x13
-#define B_TXT_CURRENT_MOVE 0x14
-#define B_TXT_LAST_MOVE 0x15
-#define B_TXT_LAST_ITEM 0x16
-#define B_TXT_LAST_ABILITY 0x17
-#define B_TXT_ATK_ABILITY 0x18
-#define B_TXT_DEF_ABILITY 0x19
-#define B_TXT_SCR_ACTIVE_ABILITY 0x1A
-#define B_TXT_EFF_ABILITY 0x1B
-#define B_TXT_TRAINER1_CLASS 0x1C
-#define B_TXT_TRAINER1_NAME 0x1D
-#define B_TXT_LINK_PLAYER_NAME 0x1E
-#define B_TXT_LINK_PARTNER_NAME 0x1F
-#define B_TXT_LINK_OPPONENT1_NAME 0x20
-#define B_TXT_LINK_OPPONENT2_NAME 0x21
-#define B_TXT_LINK_SCR_TRAINER_NAME 0x22
-#define B_TXT_PLAYER_NAME 0x23
-#define B_TXT_TRAINER1_LOSE_TEXT 0x24
-#define B_TXT_TRAINER1_WIN_TEXT 0x25
-#define B_TXT_26 0x26
-#define B_TXT_PC_CREATOR_NAME 0x27
-#define B_TXT_ATK_PREFIX1 0x28
-#define B_TXT_DEF_PREFIX1 0x29
-#define B_TXT_ATK_PREFIX2 0x2A
-#define B_TXT_DEF_PREFIX2 0x2B
-#define B_TXT_ATK_PREFIX3 0x2C
-#define B_TXT_DEF_PREFIX3 0x2D
-#define B_TXT_TRAINER2_CLASS 0x2E
-#define B_TXT_TRAINER2_NAME 0x2F
-#define B_TXT_TRAINER2_LOSE_TEXT 0x30
-#define B_TXT_TRAINER2_WIN_TEXT 0x31
-#define B_TXT_PARTNER_CLASS 0x32
-#define B_TXT_PARTNER_NAME 0x33
-#define B_TXT_BUFF3 0x34
-#define B_TXT_ATK_TRAINER_NAME 0x35
-#define B_TXT_ATK_TRAINER_CLASS 0x36
-#define B_TXT_ATK_TEAM1 0x37 // Your/The opposing
-#define B_TXT_ATK_TEAM2 0x38 // your/the opposing
-#define B_TXT_DEF_NAME 0x39
-#define B_TXT_DEF_TEAM1 0x3A // Your/The opposing
-#define B_TXT_DEF_TEAM2 0x3B // your/the opposing
-#define B_TXT_ACTIVE_NAME 0x3C
-#define B_TXT_ACTIVE_NAME2 0x3D // no Illusion check
+#define B_TXT_PLAYER_MON3_NAME 0x9
+#define B_TXT_OPPONENT_MON3_NAME 0xA
+#define B_TXT_LINK_PLAYER_MON1_NAME 0xB
+#define B_TXT_LINK_OPPONENT_MON1_NAME 0xC
+#define B_TXT_LINK_PLAYER_MON2_NAME 0xD
+#define B_TXT_LINK_OPPONENT_MON2_NAME 0xE
+#define B_TXT_ATK_NAME_WITH_PREFIX_MON1 0xF
+#define B_TXT_ATK_PARTNER_NAME 0x10
+#define B_TXT_ATK_NAME_WITH_PREFIX 0x11
+#define B_TXT_DEF_NAME_WITH_PREFIX 0x12
+#define B_TXT_EFF_NAME_WITH_PREFIX 0x13 // EFF = short for gEffectBank
+#define B_TXT_ACTIVE_NAME_WITH_PREFIX 0x14
+#define B_TXT_SCR_ACTIVE_NAME_WITH_PREFIX 0x15
+#define B_TXT_CURRENT_MOVE 0x16
+#define B_TXT_LAST_MOVE 0x17
+#define B_TXT_LAST_ITEM 0x18
+#define B_TXT_LAST_ABILITY 0x19
+#define B_TXT_ATK_ABILITY 0x1A
+#define B_TXT_DEF_ABILITY 0x1B
+#define B_TXT_SCR_ACTIVE_ABILITY 0x1C
+#define B_TXT_EFF_ABILITY 0x1D
+#define B_TXT_TRAINER1_CLASS 0x1E
+#define B_TXT_TRAINER1_NAME 0x1F
+#define B_TXT_LINK_PLAYER_NAME 0x20
+#define B_TXT_LINK_PARTNER_NAME 0x21
+#define B_TXT_LINK_OPPONENT1_NAME 0x22
+#define B_TXT_LINK_OPPONENT2_NAME 0x23
+#define B_TXT_LINK_SCR_TRAINER_NAME 0x24
+#define B_TXT_PLAYER_NAME 0x25
+#define B_TXT_TRAINER1_LOSE_TEXT 0x26
+#define B_TXT_TRAINER1_WIN_TEXT 0x27
+#define B_TXT_26 0x28
+#define B_TXT_PC_CREATOR_NAME 0x29
+#define B_TXT_ATK_PREFIX1 0x2A
+#define B_TXT_DEF_PREFIX1 0x2B
+#define B_TXT_ATK_PREFIX2 0x2C
+#define B_TXT_DEF_PREFIX2 0x2D
+#define B_TXT_ATK_PREFIX3 0x2E
+#define B_TXT_DEF_PREFIX3 0x2F
+#define B_TXT_TRAINER2_CLASS 0x30
+#define B_TXT_TRAINER2_NAME 0x31
+#define B_TXT_TRAINER2_LOSE_TEXT 0x32
+#define B_TXT_TRAINER2_WIN_TEXT 0x33
+#define B_TXT_PARTNER_CLASS 0x34
+#define B_TXT_PARTNER_NAME 0x35
+#define B_TXT_BUFF3 0x36
+#define B_TXT_ATK_TRAINER_NAME 0x37
+#define B_TXT_ATK_TRAINER_CLASS 0x38
+#define B_TXT_ATK_TEAM1 0x39 // Your/The opposing
+#define B_TXT_ATK_TEAM2 0x3A // your/the opposing
+#define B_TXT_DEF_NAME 0x3B
+#define B_TXT_DEF_TEAM1 0x3C // Your/The opposing
+#define B_TXT_DEF_TEAM2 0x3D // your/the opposing
+#define B_TXT_ACTIVE_NAME 0x3E
+#define B_TXT_ACTIVE_NAME2 0x3F // no Illusion check
 
 // for B_TXT_BUFF1, B_TXT_BUFF2 and B_TXT_BUFF3
 
@@ -216,7 +218,7 @@ struct BattleMsgData
     u8 hpScale;
     u8 itemEffectBattler;
     u8 moveType;
-    u16 abilities[MAX_BATTLERS_COUNT];
+    u8 abilities[MAX_BATTLERS_COUNT];
     u8 textBuffs[3][TEXT_BUFF_ARRAY_COUNT];
 };
 