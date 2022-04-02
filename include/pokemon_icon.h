87e9c3dfa800cc57c9c2b47d2f5d71e511ed432b
Lawrence Li <16lawrencel@gmail.com>
2022-04-02 01:31:41 -0400

    Add battle icons to code

--- include/pokemon_icon.h
+++ include/pokemon_icon.h
@@ -4,6 +4,7 @@
 extern const u8 gMonIconPaletteIndices[];
 extern const u8 gMonIconPaletteIndicesFemale[];
 extern const u8 *const gMonIconTable[];
+extern const u8 *const gMonBattleIconTable[];
 extern const struct SpritePalette gMonIconPaletteTable[];
 
 const u8 *GetMonIconTiles(u16 species, u32 personality);
